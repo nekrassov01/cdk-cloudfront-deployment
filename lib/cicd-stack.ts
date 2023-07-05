54264344d3ad3c1085f3aba012dbc190559697c1
kawashima <kawashima@cloudpack.jp>
2023-07-05 12:32:39 +0900

    fix: add ListContinuousDeploymentPolicies to FrontendPurgeProjectRole

--- lib/cicd-stack.ts
+++ lib/cicd-stack.ts
@@ -637,7 +637,11 @@ export class CloudFrontDeploymentSampleCicdStack extends Stack {
               }),
               new iam.PolicyStatement({
                 effect: iam.Effect.ALLOW,
-                actions: ["cloudfront:GetContinuousDeploymentPolicy", "cloudfront:DeleteContinuousDeploymentPolicy"],
+                actions: [
+                  "cloudfront:GetContinuousDeploymentPolicy",
+                  "cloudfront:DeleteContinuousDeploymentPolicy",
+                  "cloudfront:ListContinuousDeploymentPolicies",
+                ],
                 resources: [`arn:aws:cloudfront::${this.account}:continuous-deployment-policy/*`],
               }),
               new iam.PolicyStatement({
