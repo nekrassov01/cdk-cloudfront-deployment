86cbc38157055832cc6fe5100eb799e126b07bd2
kawashima <kawashima@cloudpack.jp>
2023-07-05 21:16:15 +0900

    style: change syntax highlight theme

--- src/s3/hosting/src/App.js
+++ src/s3/hosting/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
 import json from "react-syntax-highlighter/dist/esm/languages/hljs/json";
-import { githubGist } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { dracula } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import "./App.css";
 
 SyntaxHighlighter.registerLanguage("json", json);
@@ -9,7 +9,7 @@ SyntaxHighlighter.registerLanguage("json", json);
 function App() {
   return (
     <div className="container">
-      <SyntaxHighlighter language="json" style={githubGist}>
+      <SyntaxHighlighter language="json" style={dracula}>
         {JSON.stringify({ version: process.env.REACT_APP_VERSION_FRONTEND }, null, 2)}
       </SyntaxHighlighter>
     </div>
